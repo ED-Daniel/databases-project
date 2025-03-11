4e7bbbbca99d270e1850c0bd8a410bde005128b9
Daniel Emelyanenko <ed.daniel01@gmail.com>
2025-03-11 14:50:01 +0300

    added db init sh

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export PGPASSWORD="$POSTGRES_PASSWORD"
+
+# Ждём запуск PostgreSQL
+echo "Ожидание запуска PostgreSQL..."
+until pg_isready -h postgres -p 5432 -U test; do
+  sleep 2
+done
+
+echo "PostgreSQL запущен! Выполняем SQL-скрипты..."
+
+# Выполняем скрипты
+psql -h postgres -U test -d bdsm -f /scripts/init.sql
+psql -h postgres -U test -d bdsm -f /scripts/import.sql
+
+for sql_file in /scripts/procedures/*.sql; do
+  echo "Выполняем $sql_file..."
+  psql -h postgres -U test -d bdsm -f "$sql_file"
+done
+
+for sql_file in /scripts/views/*.sql; do
+  echo "Выполняем $sql_file..."
+  psql -h postgres -U test -d bdsm -f "$sql_file"
+done
+
+for sql_file in /scripts/materialized_views/*.sql; do
+  echo "Выполняем $sql_file..."
+  psql -h postgres -U test -d bdsm -f "$sql_file"
+done
+
+echo "SQL-скрипты выполнены успешно!"
---
+++ scripts/views/4.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_referrers AS
+SELECT 
+    u.id AS referrer_id,
+    u.email AS referrer_email,
+    COUNT(r.referred_id) AS total_referrals,
+    COALESCE(SUM(o.amount), 0) AS total_revenue
+FROM users u
+JOIN referrals r ON u.id = r.referrer_id
+LEFT JOIN orders o ON r.referred_id = o.user_id
+GROUP BY u.id
+ORDER BY total_referrals DESC, total_revenue DESC;
